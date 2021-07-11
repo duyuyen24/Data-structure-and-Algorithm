e215c9a969105e60b5e2d11d906568f3b250a14a
duyuyen24 <duyuyenphanbinh@email.com>
2021-07-11 22:09:13 +0700

    edit

--- source/app/BFS/Makefile.mk
+++ source/app/BFS/Makefile.mk
@@ -6,3 +6,7 @@ OBJ += $(OBJ_DIR)/GuiltyPrince.o
 OBJ += $(OBJ_DIR)/KefaandPark.o
 OBJ += $(OBJ_DIR)/ShortestReach.o
 OBJ += $(OBJ_DIR)/VALIDATETHEMAZE.o
+OBJ += $(OBJ_DIR)/KnightLonaChessboard.o
+OBJ += $(OBJ_DIR)/IceCave.o
+OBJ += $(OBJ_DIR)/RedKShortestPath.o
+OBJ += $(OBJ_DIR)/slick.o
